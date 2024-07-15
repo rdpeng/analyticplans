7b81b87346b42343f0c030369be7980872b00d1b
Roger D. Peng <rdpeng@gmail.com>
2024-07-15 09:57:20 -0500

    More 1-D simulation

--- sim_datasets_grid.R
+++ sim_datasets_grid.R
@@ -76,52 +76,6 @@ gr[mu[i, -k], ] |>
 
 
 
-################################################################################
-## 1-D case with sample mean
-
-library(tidyverse)
-
-ncat <- 22
-x0 <- seq(-7, 7, len = ncat)
-x0
-nobs <- 6
-ncat^nobs |>
-    format(big.mark = ",")
-
-y <- map(seq_len(nobs), function(x) x0) |>
-    reduce(function(x, y) outer(x, y, "+")) / nobs
-dim(y)
-range(y)
-
-## y
-idx <- which(y > 4 & y < 5, arr.ind = TRUE)
-#idx <- which(y > -0.5 & y < 0.5, arr.ind = TRUE)
-nrow(idx)
-
-isamp <- idx[sample(seq_len(nrow(idx)), 20), ]
-isamp <- isamp[order(apply(isamp, 1, var)), ]
-dat <- tibble(x = apply(isamp, 1, function(j) x0[j]) |>
-                  as.vector(),
-              id = rep(1:nrow(isamp), each = nobs)) |>
-    group_by(id) |>
-    mutate(positive = ifelse(all(x > 0), "All Positive", "Some Negative"),
-           sdx = ifelse(sd(x) < 3, "Small Var.", "Large Var."))
-dat |>
-    mutate(property = interaction(positive, sdx)) |>
-    ggplot(aes(x, id)) +
-    geom_hline(aes(yintercept = id), col = "gray") +
-    geom_vline(xintercept = 0, lty = 2) +
-    geom_jitter(aes(color = sdx),
-                size = 4,
-                height = 0, width = 0.05,
-                alpha = 1/2) +
-    geom_point(aes(m, id),
-               data = dat |>
-                   group_by(id) |>
-                   summarize(m = mean(x)),
-               color = "red", size = 3, pch = 15) +
-    xlim(c(-7, 7)) +
-    theme_bw()
 
 ################################################################################
 ## 2-D?
